b1e15ab2a8b910054179750ac635d48186ef1b6e
sam-goldman <samgoldman12@gmail.com>
2022-05-02 21:32:48 -0700

    tutorial script done

--- tutorials/l2-custom-erc721/example.js
+++ tutorials/l2-custom-erc721/example.js
@@ -1,9 +1,7 @@
 const ethers = require('ethers')
-const hre = require("hardhat")
-const { predeploys } = require('@eth-optimism/contracts')
 const optimismSDK = require("@eth-optimism/sdk")
-require('dotenv').config()
 const { getContractInterface } = require('../../src')
+require('dotenv').config()
 
 async function main() {
   // Set up our RPC provider connections
@@ -13,6 +11,7 @@ async function main() {
   // Set up our wallets for interacting with L1 and L2. Both will use the same private key.
   const l1Wallet = new ethers.Wallet(process.env.PRIVATE_KEY, l1RpcProvider)
   const l2Wallet = new ethers.Wallet(process.env.PRIVATE_KEY, l2RpcProvider)
+  const walletAddr = l1Wallet.address // same as l2Wallet.address
 
   // Set up the cross chain messenger
   const crossChainMessenger = new optimismSDK.CrossChainMessenger({
@@ -22,19 +21,21 @@ async function main() {
   })
 
   // Pre-deployed addresses
+  const L2CustomERC721Addr = '0x9DFb517852D28113694eC9356cCb1643847B37c8' // contract is pre-deployed for the purpose of this tutorial
   const L1ERC721Addr = '0xA779A0cA89556A9dffD47527F0aad1c2e0d66e46'
   const L1ERC721BridgeAddr = '0xb5B701310B967d16F831C28C01EF277cCFEAC7d0'
   const L2ERC721BridgeAddr = '0xA779A0cA89556A9dffD47527F0aad1c2e0d66e46'
-  const L2StandardERC721FactoryAddr = '0xb5B701310B967d16F831C28C01EF277cCFEAC7d0'
   const L2StandardERC721Addr = '0xCA6E83201a93236f52e3726bCE97DDeBee2DBb59'
 
   // Contract setup
+  const L2CustomERC721 = new ethers.Contract(L2CustomERC721Addr, getContractInterface('L2CustomERC721'), l2Wallet)
   const L1ERC721 = new ethers.Contract(L1ERC721Addr, getContractInterface('L1ERC721'), l1Wallet)
   const L1ERC721Bridge = new ethers.Contract(L1ERC721BridgeAddr, getContractInterface('L1ERC721Bridge'), l1Wallet)
   const L2ERC721Bridge = new ethers.Contract(L2ERC721BridgeAddr, getContractInterface('L2ERC721Bridge'), l2Wallet)
-  const L2StandardERC721Factory = new ethers.Contract(L2StandardERC721FactoryAddr, getContractInterface('L2StandardERC721Factory'), l2Wallet)
   const L2StandardERC721 = new ethers.Contract(L2StandardERC721Addr, getContractInterface('L2StandardERC721'), l2Wallet)
 
+  console.log(`Minting an L1 NFT...`)
+  
   // Mint an NFT from the L1 Wallet
   const response = await L1ERC721.mint()
 
@@ -44,26 +45,12 @@ async function main() {
     ({ event }) => event === "Transfer"
   ).args.tokenId
 
-  // Log the initial balances
-  console.log(`Balance on L1: ${await L1ERC721.balanceOf(l1Wallet.address)}`) // 1
-  console.log(`Balance on L2: ${await L2StandardERC721.balanceOf(l1Wallet.address)}`) // 0
-
   // Approve the L1 bridge to transfer the NFT.
-  console.log('Approving L1 bridge to transfer NFT...')
   const response1 = await L1ERC721.approve(L1ERC721Bridge.address, tokenId)
   await response1.wait()
 
-  // Do not remove this check.
-  // It ensures that the L2 ERC721 contract is compliant and valid. If the contract doesn't implement
-  // IL2StandardERC721 or it does not correspond to the L1 NFT being deposited, no deposit will take place
-  // and the L2 Bridge will send the NFT to the L1 Bridge.
-  if (await L2StandardERC721.l1Token() != L1ERC721.address) {
-    console.log(`L2 NFT does not correspond to L1 NFT: L2StandardERC721.l1Token() = ${await L2StandardERC721.l1Token()}`)
-    process.exit(0)
-  }
-
   // Call the L1 Bridge's deposit function to begin the L1 -> L2 transfer and lock the NFT in the L1 Bridge.
-  console.log('Depositing NFT into L2. This takes about a minute.')
+  console.log('Depositing NFT into the L2 Standard ERC721 contract. This takes about a minute.')
   const response2 = await L1ERC721Bridge.depositERC721(
     L1ERC721.address,
     L2StandardERC721.address,
@@ -79,44 +66,46 @@ async function main() {
     optimismSDK.MessageStatus.RELAYED
   )
 
-  // Log the balances to see that the transfer worked! An L2 NFT was minted and sent to the owner.
-  // The L1 NFT will now be held by the L1 Bridge until the L2 NFT is withdrawn from L2.
-  console.log(`Balance on L1: ${await L1ERC721.balanceOf(l1Wallet.address)}`) // 0
-  console.log(`Balance on L2: ${await L2StandardERC721.balanceOf(l1Wallet.address)}`) // 1
+  // The L1 -> L2 deposit is now complete! An NFT was minted from the L2 Standard ERC721 contract and sent to the owner.
+  console.log(`Deposit successful. Balance of owner on L2 Standard ERC721: ${await L2StandardERC721.balanceOf(walletAddr)}`) // 1
 
-  // Withdraw the NFT on L2. This burns the L2 NFT and sends a message to the L1 contract to unlock
-  // the L1 NFT and send it back to the owner.
-  console.log(`Withdrawing NFT back to L1...`)
-  const response3 = await L2ERC721Bridge.withdraw(
-    L2StandardERC721.address,
+  console.log(`Transferring NFT from the Standard ERC721 contract to the Custom ERC721 contract...`)
+
+  // Transfer ownership of the NFT from the L2 wallet to the Custom contract. We must use safeTransferFrom here because
+  // it implicitly calls the onERC721Received function of the Custom contract, which in turn mints a new NFT from the
+  // Custom contract and sends it to the owner.
+  const response3 = await L2StandardERC721["safeTransferFrom(address,address,uint256)"](
+    walletAddr,
+    L2CustomERC721.address,
     tokenId,
-    2000000,
-    '0x'
   )
   await response3.wait()
 
-  // Wait for the message to be relayed to L1.
-  console.log(`Waiting until withdrawal is ready to be relayed. This takes a couple minutes on Kovan (and a week on mainnet!).`)
-  await crossChainMessenger.waitForMessageStatus(
-    response3.hash, 
-    optimismSDK.MessageStatus.READY_FOR_RELAY
-  )
+  console.log(`Transfer successful. The Custom contract automatically minted an NFT and sent it to the owner:`)
 
-  // Now that the message is ready to be relayed, we can finalize the transaction on L1.
-  // This requires a second transaction, which can only be executed after a week on mainnet.
-  // See more here: https://community.optimism.io/docs/developers/bridge/messaging/#for-l2-%E2%87%92-l1-transactions
-  await crossChainMessenger.finalizeMessage(response3)
+  // A new NFT with the same token ID has been minted from the Custom contract and sent to the owner.
+  // The Custom contract now owns the NFT sent from the Standard contract.
+  console.log(`Balance of owner on L2 Custom ERC721: ${await L2CustomERC721.balanceOf(walletAddr)}`) // 1
+  console.log(`Balance of owner on L2 Standard ERC721: ${await L2StandardERC721.balanceOf(walletAddr)}`) // 0
+  
+  console.log(`Withdrawing the NFT to L1...`)
 
-  console.log(`Waiting for relay to be finalized...`)
-  await crossChainMessenger.waitForMessageStatus(
-    response3,
-    optimismSDK.MessageStatus.RELAYED
-  )
+  // Withdraws the NFT to L1, which burns the both the NFT that is held in the Custom contract as well as the
+  // owner's NFT, which was minted from the Custom contract. This totally erases the presence of the NFT on L2.
+  const response4 = await L2CustomERC721.withdrawToL1(walletAddr, tokenId)
+  await response4.wait()
 
-  // Log balances again! The L2 NFT has now been burned, and the L1 NFT has been released
-  // back to its owner.
-  console.log(`Balance on L1: ${await L1ERC721.balanceOf(l1Wallet.address)}`) // 1
-  console.log(`Balance on L2: ${await L2StandardERC721.balanceOf(l1Wallet.address)}`) // 0
+  console.log(`Withdraw to L1 successfully initiated!`)
+
+  // Checks that the NFT has successfully been removed from the Custom contract.
+  await L2CustomERC721.ownerOf(tokenId).then(() => {
+    console.log(`Something went wrong! NFT not removed from Custom contract.`)
+  }).catch(() => {})
+
+  // Checks that NFT has suceessfully been removed from Custom contract.
+  await L2StandardERC721.ownerOf(tokenId).then(() => {
+    console.log(`Something went wrong! NFT not removed from Standard contract.`)
+  }).catch(() => {})
 }
 
 main()
